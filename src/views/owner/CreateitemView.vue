35767512d70dd09dd70a03bd4de26c45bc1746d2
Jens Bengtsson <jb225je@student.lnu.se>
2024-12-09 12:42:11 +0100

    outcomment stuff

--- src/views/owner/CreateitemView.vue
+++ src/views/owner/CreateitemView.vue
@@ -121,7 +121,7 @@ const getInputValue = () => {
 </template>
 
 <style scoped>
-.pub-container {
+/* .pub-container {
     border: 2px solid #4CAF50;
     border-radius: 10px;
     padding: 1.5rem;
@@ -132,8 +132,7 @@ const getInputValue = () => {
     font-family: Arial, sans-serif;
     color: #333;
     text-align: center;
-    /* Centrerar innehållet */
-}
+} */
 
 .modal-backdrop {
     position: fixed;
