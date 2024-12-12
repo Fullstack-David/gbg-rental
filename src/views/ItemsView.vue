602fbf42db69c8a08f28f48700c075708e9a601e
Nyat Ghirmazion <nyat.ghirmazion@chasacademy.se>
2024-12-12 09:06:22 +0100

    delete funktionalitet och id

--- src/views/ItemsView.vue
+++ src/views/ItemsView.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { useItems } from "@/composables/useItems";
 import { ref, onMounted } from "vue";
+import { itemsApi } from '@/services/itemsApi';
 
 const { items, isLoading, fetchItems } = useItems();
 
@@ -39,6 +40,23 @@ const bookItem = () => {
   closeBookingForm(); // Stänger formuläret efter bokningen
 };
 
+// Ta bort en annons
+const deleteItem = async (id) => {
+  try {
+    const confirmed = confirm('Är du säker på att du vill ta bort denna annons?');
+    if (!confirmed) return;
+
+    const updatedItems = await itemsApi.deleteItem(id);
+
+    items.value = updatedItems
+
+    alert('Annonsen har tagits bort');
+  } catch (error) {
+    console.error('kunde inte ta bort annonsen:', error);
+    alert('Ett fel inträffade, försök igen senare')
+  }
+}
+
 onMounted(() => {
   fetchItems();
 });
@@ -64,6 +82,7 @@ onMounted(() => {
          
         <button @click="openBookingForm(item)">Boka</button>
         <!-- Booking Button -->
+        <button class="dlt-btn" @click="deleteItem(item.id)" >Ta bort annons</button>
       </div>
     </div>
 
@@ -137,6 +156,20 @@ button {
 
 button:hover {
   background-color: #45a049;
+}
+
+.dlt-btn {
+  background-color: #a82828;
+  color: white;
+  border: none;
+  padding: 0.5rem 1rem;
+  border-radius: 0.5rem;
+  cursor: pointer;
+  margin: 10px 0;
+}
+
+.dlt-btn:hover {
+  background-color: #7a0e0e;
 }
 
 h3 {
